f17e589fb27c9f395689e363bee2bdca3d7d8530
ani67h <ghoshanindya4589@gmail.com>
2025-07-31 23:48:51 +0600

    producttuple(edited)

--- tupleproduct.py
+++ tupleproduct.py
@@ -10,6 +10,6 @@ print("The tupple is ",numtup)
 
 multiply = 1
 
-for i in numtup :
+for i in numtup : # here range is not needed in the for loop as we are mentioning it to get inside and access the numbers inside the tuple
     multiply *= i
 print("The product of the tuple is ",multiply)    
