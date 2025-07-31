85fac189e758a1acfdafbebe13e7a1b51812aff5
ani67h <ghoshanindya4589@gmail.com>
2025-07-31 23:12:34 +0600

    tuplechange

--- operationtuple.py
+++ operationtuple.py
@@ -4,7 +4,7 @@ print(tuplex)
 
 # creating a tuple
 tuplex = (4, 6, 2, 8, 3, 1)
-print(tuple)
+print(tuplex)
 # tuples are immutable, so you can not add new elements
 # using merge of tuples with the + operator you can add an element and it will create a new tuple
 tuplex = tuplex + (9,)
